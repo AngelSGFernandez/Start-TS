af2592e233eab00750d67142f46612cb76f6124e
AngelSGFernandez <angel.fdz@mail.com>
2022-01-02 02:47:16 -0600

    01-basicTypes

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,18 @@
     ===== Código de TypeScript =====
 */
 
-    
+    let userName: string = "Angel";
+    let healthPoints: number | string = 95;
+    let state: boolean = true;
+
+    healthPoints = "noventa y cinco";
+
+    console.log(userName, " ", healthPoints, " ", state, " ")
+
+    const app = document.getElementById("app");
+
+    const p = document.createElement("p");
+
+    p.textContent = "HOLA!"
+
+    app.appendChild(p);
