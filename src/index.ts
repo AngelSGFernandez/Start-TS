fc5e429acf21b311f3b017707b6c12c5a8b4e0d5
AngelSGFernandez <angel.fdz@mail.com>
2022-01-06 02:39:31 -0600

    Add Destructuring Objects

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,35 @@
     ===== Código de TypeScript =====
 */
 
+interface Player {
+    volume: number;
+    second: number;
+    song: string;
+    details: Details;
+}
+
+interface Details {
+    author: string;
+    year: number;
+}
+
+const player : Player = {
+    volume: 100,
+    second: 45,
+    song: 'In The End',
+    details: {
+        author: 'Linkin Park',
+        year: 2001
+    }
+}
+
+const author: string = 'Jhon Doe';
+
+const { volume, second, song, details } = player;
+const { author: authorSong } = details;
+
+console.log( 'The current volume is: ', volume );
+console.log( 'The current second is: ', second );
+console.log( 'The current song is: ', song );
+console.log( 'The author is: ', authorSong );
+
