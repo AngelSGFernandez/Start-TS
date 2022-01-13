bb25f1e21ca60c5df00d45e207cb386989ac276c
AngelSGFernandez <angel.fdz@mail.com>
2022-01-13 01:30:35 -0600

    Add basic classes

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,15 @@
 */
 
 
+class Hero {
+     alterEgo: string;
+     age: number;
+     realName: string;
+
+    printName() {
+        return this.alterEgo + ' ' + this.realName;
+    }
+}
+
+
+ const superman = new Hero();
